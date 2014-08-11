f256058ed26412ef2b64f98c759b13da970ddef7
Augusto <agu.amarilla@gmail.com>
2014-08-11 13:36:01 -0400

    Agregada la libreria javax.json 1.0.4

---
+++ tarea1/src/java/py/una/pol/progweb/Message.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package py.una.pol.progweb;
+
+/**
+ *
+ * @author augusto
+ */
+import java.io.StringWriter;
+import javax.json.Json;
+import javax.json.JsonObject;
+ 
+public class Message {
+    private JsonObject json;
+ 
+    public Message(JsonObject json) {
+        this.json = json;
+    }
+ 
+    public JsonObject getJson() {
+        return json;
+    }
+ 
+    public void setJson(JsonObject json) {
+        this.json = json;
+    }
+ 
+    @Override
+    public String toString(){
+        StringWriter writer = new StringWriter();
+ 
+        Json.createWriter(writer).write(json);
+ 
+        return writer.toString();
+    }
+ 
+}
